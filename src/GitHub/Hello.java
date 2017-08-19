4d4a83e565ff982a18270cbaca6c1e012e0a82e5
priyankanagraj <31151842+priyankanagraj@users.noreply.github.com>
2017-08-19 11:32:52 +0530

    Create Hello.java

---
+++ src/GitHub/Hello.java
@@ -0,0 +1,13 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Hello{
+
+	@Test
+	public void A(){
+		
+		Reporter.log("pulling from git",true);
+	}
+	
