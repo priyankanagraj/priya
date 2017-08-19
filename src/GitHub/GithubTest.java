07665bf83368cd24bb5cb1dddb330ab4a506846b
manasvin <manasvin@LAPTOP-TKI5RET1>
2017-08-19 11:23:37 +0530

    hi

---
+++ src/GitHub/GithubTest.java
@@ -0,0 +1,16 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubTest {
+
+	@Test
+	public void GithubCheckIn(){
+		
+		Reporter.log("Pushing code to Github",true);
+	}
+	
+	
+	
+}
